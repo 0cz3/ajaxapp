6cc1474a188869441139064fe44e93048f1468ee
0cz <sumire.hamaguchi@nijibox.co.jp>
2024-03-18 22:28:16 +0900

    データを表示 viewを定義

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ function fetchUserInfo(userId) {
                 console.error("エラーレスポンス", response);
             } else {
                 return response.json().then(userInfo => {
-                    console.log(userInfo);
+                    const view = `
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                    `;
                 });
             }
         }).catch(error => {

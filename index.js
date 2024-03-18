997d76fc0aca7cfa864ad11fd92dfd2973cc1e63
0cz <sumire.hamaguchi@nijibox.co.jp>
2024-03-18 11:23:47 +0900

    HTTP通信 fetch呼び出し時にuseIdを指定する記述へ変更

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const userId = "0cz";
-fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then(response => {
-        console.log(response.status); // => 200
-        // エラーレスポンスが返されたことを検知する
-        if (!response.ok) {
-            console.error("エラーレスポンス", response);
-        } else {
-            return response.json().then(userInfo => {
-                console.log(userInfo);
-            });
-        }
-    }).catch(error => {
-        console.error(error);
-    });
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status);
+            // エラーレスポンスが返されたことを検知する
+            if (!response.ok) {
+                console.error("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}

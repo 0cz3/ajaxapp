02c471dd074246cf7f95cf501d891592b6933cf9
0cz <sumire.hamaguchi@nijibox.co.jp>
2024-03-18 11:04:22 +0900

    HTTP通信 fetchの記述を追加

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("index.js: loaded");
+const userId = "0cz";
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+        console.log(response.status); // => 200
+        return response.json().then(userInfo => {
+            // JSONパースされたオブジェクトが渡される
+            console.log(userInfo); // => {...}
+        });
+    });
